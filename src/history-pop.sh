b43fcb3ee44b2ae0c4cf0e644b01eba79a02e85b
Erik Dubois <awa22@fsu.edu>
2024-09-15 11:36:22 -0400

    Made clearing notifications actually possible, just click em!

--- src/history-pop.sh
+++ src/history-pop.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Fetch notifications and their IDs
+notifications=$(dunstctl history | jq -r '.data[].[].body.data // empty | select(length > 0)')
+ids=$(dunstctl history | jq -r '.data[].[].id.data')
 
-options=$(dunstctl history | jq -r '.data[].[].body.data // empty | select(length > 0)' | awk 'NF')
-options_str=$(printf "%s\n" "$options")
+# Combine notifications with their corresponding IDs
+menu_items=""
+index=0
+while IFS= read -r notif && IFS= read -r id <&3; do
+    menu_items+="$notif : $id\n"
+    ((index++))
+done < <(echo "$notifications") 3< <(echo "$ids")
 
-# Add custom options
-#custom_options="Open Terminal\nRestart Window Manager\nShutdown\nReboot"
 
-# Combine notifications and custom options
-menu_items=$(printf "%s\n%s" "$options_str")
-
-# Use rofi to display the options and get the selected option
 selected=$(echo -e "$menu_items" | rofi -dmenu -p "Select an option:")
+
+if [[ $selected == *:* ]]; then
+    id=$(echo "$selected" | awk -F':' '{print $2}')
+    dunstctl history-rm "$id"
+fi
